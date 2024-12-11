14dd2685c62c5f0772997ec35dccf5ffb6a4d37e
Abdul Mohiz <142517175+AbdulMohizSultan@users.noreply.github.com>
2024-12-11 18:49:22 +0500

    project 3 complete

--- Week 3/Dom/DOM Five project/3 project/cursor.js
+++ Week 3/Dom/DOM Five project/3 project/cursor.js
@@ -1,6 +1,7 @@
 var main = document.querySelector('#main')
 var crsr = document.querySelector('.cursor')
 
-main.addEventListener('mousemoove', function(){
-    console.log('hey')
+main.addEventListener('mousemove', function(dets){
+    crsr.style.left = dets.x + 'px'
+    crsr.style.top = dets.y + 'px'
 })
