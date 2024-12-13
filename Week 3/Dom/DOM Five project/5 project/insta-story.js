e764f39a12e376ee923fdb835b0e5c09177c7829
Abdul Mohiz <142517175+AbdulMohizSultan@users.noreply.github.com>
2024-12-13 20:43:31 +0500

    update

--- Week 3/Dom/DOM Five project/5 project/insta-story.js
+++ Week 3/Dom/DOM Five project/5 project/insta-story.js
@@ -1,8 +1,21 @@
-var arr = [1,2,3,5]
+var arr = [
+    { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4x1uCKPCMKyHbDixV3Jfdrpsngf2lnONrzg&s" ,
+      story: "https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4x1uCKPCMKyHbDixV3Jfdrpsngf2lnONrzg&s" ,
+      story: "https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS28kRh_fU98N_6B0uX_AhlVHOJPv7SPTD6nw&s" ,
+      story: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3W467LCoHkFmWNBQywa3B3jS3-0gBzN1lAw&s" ,
+      story: "https://images.pexels.com/photos/919073/pexels-photo-919073.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
 
-var clutter = ''
-arr.forEach( function() {
-    clutter += 'hello' 
+
+]
+
+arr.forEach( function(elem) {
+      console.log(elem)
 })
 
-console.log(clutter)
