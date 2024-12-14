73694783ac24d03496d86820ac59ed34accdd58d
Abdul Mohiz <142517175+AbdulMohizSultan@users.noreply.github.com>
2024-12-14 21:07:33 +0500

    complete project 4

--- Week 3/Dom/DOM Five project/5 project/insta-story.js
+++ Week 3/Dom/DOM Five project/5 project/insta-story.js
@@ -2,7 +2,7 @@ var arr = [
     { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4x1uCKPCMKyHbDixV3Jfdrpsngf2lnONrzg&s" ,
       story: "https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=400"
     },
-    { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4x1uCKPCMKyHbDixV3Jfdrpsngf2lnONrzg&s" ,
+    { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5r82bJTGqr9YmMiAur8rnh41AsmUzWiSOdA&s" ,
       story: "https://images.pexels.com/photos/1545743/pexels-photo-1545743.jpeg?auto=compress&cs=tinysrgb&w=400"
     },
     { dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS28kRh_fU98N_6B0uX_AhlVHOJPv7SPTD6nw&s" ,
@@ -14,8 +14,23 @@ var arr = [
 
 
 ]
+var storiyan = document.querySelector('#storiyan')
 
-arr.forEach( function(elem) {
-      console.log(elem)
+var cultter = ""
+arr.forEach( function(elem,idx) {
+      cultter += `<div id="story">
+                    <img id="${idx}" src="${elem.dp}" alt="">
+                  </div>`
 })
 
+storiyan.innerHTML = cultter ;
+
+storiyan.addEventListener('click', function(dets){
+    document.querySelector("#full-screen").style.display = 'block'
+
+    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+    setTimeout(function(){
+      document.querySelector("#full-screen").style.display = 'none'
+    }, 4000)
+})
