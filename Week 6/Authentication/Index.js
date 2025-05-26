86b684ac5ce14a0de3c59e183251b0197faeec04
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-05-26 21:03:47 +0500

    Create a signin and signup system

---
+++ Week 6/Authentication/Index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { use } = require('react');
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+// should return a random long stribing
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+
+app.post("/signup", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+
+    users.push({
+        username: username,
+    })
+
+    res.json({
+        message:"you are signed in"
+    })
+})
+
+
+app.post("/signin", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // map and filter
+
+    let foundUsers = null;
+
+    for(let i = 0; i<users.length; i++){
+        if(users[i].username == username && user[i].password == password) {
+            foundUsers = users[i]
+        }
+    }
+
+    if(foundUsers) {
+        const token = generateToken();
+        foundUsers.token = token;
+        res.json({
+            token:token
+        })
+    } else {
+        res.status(403).send({
+            message:"Invaltd username or password"
+        })
+    }
+})
+
+
+app.listen(3000);
