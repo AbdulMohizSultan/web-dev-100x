774f3ac1b53b38c3f5457482e119695fd1d55bc7
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-05-28 21:57:47 +0500

    today

--- Week 6/Authentication/Index.js
+++ Week 6/Authentication/Index.js
@@ -61,5 +61,26 @@ app.post("/signin", function(req, res){
     }
 })
 
+app.get("/me", function(req,res){
+    const token = req.headers.token
+    let foundUser = null;
+
+    for(let i =0; i < users.length; i++){
+        if(users[i].token == token) {
+            foundUser = users[i]
+        }
+    }
+
+    if(foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.json({
+            message:"token invalied"
+        })
+    }
+})
 
 app.listen(3000);
