c57391de0ae39e05bf8a12a87496c71bdf87b8ad
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-06-04 23:11:29 +0500

    sum Assignment

--- Week 6/Project-Of-Login/index.js
+++ Week 6/Project-Of-Login/index.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "randommohiz "
+const { use } = require('react');
 
 const app = express();
 app.use(express.json());
@@ -21,9 +23,78 @@ app.post("/sigup", function(req, res){
 })
 
 app.post("/sigin", function(req, res){
+       const  username = req.body.username;
+       const  password = req.body.password;
+       
+       let foundUser = null;
 
+       for(let i = 0; i< username === username ) {
+        if(users[i].username === username && users[i].password === password) {
+            foundUser = users[i]
+        }
+       }
+
+       if (!foundUser){
+        res.json({
+            message: "credentiate incorrect"
+        })
+        return
+      }else {
+       const token = jwt.sign({
+        username
+       }, JWT_SECRET);
+
+       res.json({
+        token: token
+       })
+    }
+    
 })
 
-app.get("/me", function(req, res){
+app.get("/me", auth, function(req, res){
+    // const token = req.headers.token;
 
+    // const decodedDate = jwt.verify(token, JWT_SECRET);
+
+    // if (decodedDate.username) {
+        let foundUser = null; 
+
+        for (let i =0; i < users.length; i++){
+            if(users[i].username === username){
+                foundUser = users[i]
+            }
+        }
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    }
+)
+
+function auth (req , res , next){
+
+    const token = req.headers.token;
+    const decodedDate = jwt.verify(token, JWT_SECRET);
+    if(decodedDate.username){
+    next()
+    } else {
+        res.json({
+            message: "use"
+        })
+    }
+}
+
+app.get("/todo", auth, function(req , res){
+    
 })
+
+app.get("/todo", auth, function(req , res){
+    
+})
+
+
+app.get("/todo", auth, function(req , res){
+    
+})
+
+app.listen(3000);
