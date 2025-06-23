433c1e114c84e22122a1b25c304d71cc916c760e
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-06-23 23:08:46 +0500

    jwt

--- Week 6/Project-Of-Login/My self Project/second project/app.js
+++ Week 6/Project-Of-Login/My self Project/second project/app.js
@@ -1,2 +1,17 @@
 const express  = require('express');
-const app = express
+const cookiePraser = require('cookie-parser') 
+const bcrypt = require("bcrypt");
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.use(cookiePraser());
+
+app.get("/", function(req,res){
+
+    let token = jwt.sign({email: "mohiz@gmai.com"}, "scert");
+    res.cookie("token", token)
+    res.send("dones")
+})
+
+
+app.listen(3006);
