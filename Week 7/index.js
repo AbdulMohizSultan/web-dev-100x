91385c65aad5969449922d1765a44c18ce860eae
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-07-16 22:39:30 +0500

    mongo da schema

---
+++ Week 7/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const {UserModel, TodoModel} = require("./db");
+const { message } = require("statuses");
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    await UserModel.insert({
+          email:email,
+          password : password,
+          name:name
+    })
+
+    res.json({
+        message:"You are logged in"
+    })
+});
+
+app.post("signin", function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = UserModel.findOne({
+        email:email,
+        password:password
+    })
+
+    if(user) {
+        const token ="";
+        res.json({
+
+    })
+    }else {
+        res.status(403).json({
+            message: "incorrect credentials"
+        })
+    }
+});
+
+app.post("/todo", function(req, res){
+
+});
+
+app.get("/todos", function(req, res){
+
+});
+
+
+app.listen(3000)
