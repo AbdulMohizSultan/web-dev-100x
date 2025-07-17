4c27c0e8d96da17f29ba724cc2383ffda18b67d8
Shoaib Sultan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-07-17 22:17:37 +0500

    create some thimg

--- Week 7/index.js
+++ Week 7/index.js
@@ -3,38 +3,49 @@ const express = require("express");
 const {UserModel, TodoModel} = require("./db");
 const { message } = require("statuses");
 const app = express();
+const jwt = require("jsonwebtoken");
+const { use } = require("react");
+const  mongoose = require("mongoose");
+const JWT_SECRET = "asdasd12@123";
+
+
+mongoose.connect("mongodb+srv://abdulmohiz1000:mohiz%4012345@cluster0.o1rssvc.mongodb.net/todo-app-database")
 
 app.use(express.json());
 
-app.post("/signup", async function(req, res){
+app.post("/signup", async function(req, res) {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    await UserModel.insert({
-          email:email,
-          password : password,
-          name:name
-    })
-
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    });
+    
     res.json({
-        message:"You are logged in"
+        message: "You are signed up"
     })
 });
 
-app.post("signin", function(req, res){
+app.post("/signin ", async  function(req, res){
     const email = req.body.email;
     const password = req.body.password;
 
-    const user = UserModel.findOne({
+    const user = await UserModel.findOne({
         email:email,
         password:password
     })
 
-    if(user) {
-        const token ="";
-        res.json({
+ console.log(user)
 
+    if(user) {
+        const token = jwt.sign({
+            id:user._id
+        }, JWT_SECRET);
+        res.json({
+          token:token
     })
     }else {
         res.status(403).json({
