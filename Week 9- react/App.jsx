89aea8ec2ce21baf4e7c21f4a36c8e46d57c8ec9
Abdul Mohiz Awan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-09-17 10:23:40 -0700

    create a counter with react

--- Week 9- react/App.jsx
+++ Week 9- react/App.jsx
@@ -1,17 +1,38 @@
-export default function App() {
+// export default function App() {
 
-    function onClickHandler () {
-        const currentValue = document.getElementById("btn").innerHTML;
-        const currentCounter = currentValue.split("")[1];
-        const newCounter = parseInt(currentCounter) + 1 ;
-        document.getElementById("btn").innerHTML = "Counter" + newCounter;
+//     function onClickHandler () {
+//         const currentValue = document.getElementById("btn").innerHTML;
+//         const currentCounter = currentValue.split("")[1];
+//         const newCounter = parseInt(currentCounter) + 1 ;
+//         document.getElementById("btn").innerHTML = "Counter" + newCounter;
+//     }
+
+
+
+//     return (
+//         <div>
+//            <button onClick={onClickHandler} > Counter 1  </button>
+//         </div>
+//     )
+// }
+
+
+import {useState} from "react";
+
+
+export default function App()   {
+    const [count, setCount] = useState(0);
+
+
+    function onClickHandler() {
+        setCount(count + 1);
     }
-
-
 
     return (
         <div>
-           <button onClick={onClickHandler} > Counter 1  </button>
+            <button id="btn" onClick={onClickHandler}>
+                Counter {count}
+            </button>
         </div>
     )
 }
