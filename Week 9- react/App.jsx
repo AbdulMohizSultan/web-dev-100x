851712746735a958492cb5436eb8ccb339e84eb2
Abdul Mohiz Awan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-09-18 10:03:18 -0700

    create a todo with react

--- Week 9- react/App.jsx
+++ Week 9- react/App.jsx
@@ -1,5 +1,8 @@
 // export default function App() {
 
+import { title } from "process";
+import { useState } from "react";
+
 //     function onClickHandler () {
 //         const currentValue = document.getElementById("btn").innerHTML;
 //         const currentCounter = currentValue.split("")[1];
@@ -17,22 +20,69 @@
 // }
 
 
-import {useState} from "react";
+// import {useState} from "react";
 
 
-export default function App()   {
-    const [count, setCount] = useState(0);
+// export default function App()   {
+//     const [count, setCount] = useState(0);
 
 
-    function onClickHandler() {
-        setCount(count + 1);
+//     function onClickHandler() {
+//         setCount(count + 1);
+//     }
+
+//     return (
+//         <div>
+//             <button id="btn" onClick={onClickHandler}>
+//                 Counter {count}
+//             </button>
+//         </div>
+//     )
+// }
+ 
+
+
+
+export default function App() {
+    const [todos, setTodos] = useState([
+        {
+        title: "Go to Gym",
+        description: "Hit the regularly",
+        done:false,
+        },
+    ]);
+
+
+    function addTodo() {
+        let newArray = [];
+        for(let i = 0; i < todos.length; i++) {
+            newArray.push(todos[i]);        
+        }
+        newArray.push({
+            title: document.getElementById("title").value  ,
+            description: document.getElementById("description").value,
+            done: true,
+        });
+        setTodos(newArray);
     }
 
     return (
         <div>
-            <button id="btn" onClick={onClickHandler}>
-                Counter {count}
-            </button>
+            <input type="text" placeholder="Title"></input>
+            <input type="text"  placeholder="description"></input>
+            <br />
+            <button onClick={addTodo}>Add todo</button>
+            <br />
+            <Todo  title={'mohiz'} description={"Abdul Mohiz"}   done={false} />
         </div>
     )
+}
+ 
+
+function Todo(props) {
+    return <div>
+        <h1>{props.title}</h1>
+        <h2>{props.description}</h2>
+        <h1>{props.done}</h1>
+    </div>
 }
