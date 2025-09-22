82b83ff1d85606739b7934ff65a00958c476707a
Abdul Mohiz Awan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-09-22 10:03:16 -0700

    Usestate and UseEffect

--- Week 9- react/react-practice/src/App.jsx
+++ Week 9- react/react-practice/src/App.jsx
@@ -35,33 +35,116 @@
 // export default App
 
 
+// conditional rendering
+
+// import { useState , useEffect } from "react";
+
+// function App() {
+//   let [counterVisiable, setCounterVisible] = useState(true); 
+    
+//     useEffect(function() {
+//       setInterval(function() {
+//         setCounterVisible(c => !c)
+//       }, 5000);
+//     }, [])
+
+//   return <div>
+//     hi
+//    {/* {counterVisiable ?  <Counter></Counter> : null } */}
+   
+//    {counterVisiable &&  <Counter></Counter>}
+//    hello
+
+//   </div>
+// }
+
+// function Counter() {
+//    const [count, setCount] = useState(0);
+   
+//   //  console.log("counter");
+
+//    useEffect(function() {
+    
+//     // setInterval(function() {
+//     //    setCount(c => c + 1)
+//     // }, 5000);
+    
+//     // console.log("hi there");
+
+    
+//     console.log(" on mount") ;
+//     let clock =  setInterval(function () {
+//       console.log("from inside interval");
+//       setCount(c => c + 1)
+//     }, 1000);
+
+//      return function() {
+//       // console.log("on unmount");  
+//       clearInterval(clock)
+//      }
+//      }, []);
+
+//     function increaseCount() {
+      
+//     console.log("unmount");
+//       setCount(count + 1)
+//     }
+
+//       return <div> 
+//      <h1>{count}</h1>
+//       <button onClick={increaseCount}>increase Count</button>
+//     </div>
+ 
+  
+//   }
+
+// export default App
+
+
 
 import { useState , useEffect } from "react";
 
 function App() {
-  return <div>
-    <Counter></Counter>
-  </div>
-}
-
-function Counter() {
-   const [count, setCount] = useState(0);
    
-   console.log("counter");
+  const [count, setCount] = useState(0) ;
+  const [count2, setCount2] = useState(0) ;
 
-   useEffect(function() {
-    setInterval(function() {
-      setCount(function(count){
-        return count + 1
-      })
-    }, 1000)
-    console.log("montued");
-   }, []);
+  function increase() {
+    setCount(c => c + 1);
+  }
 
+  
+  function decrease() {
+    setCount2(c => c 0    1);
+  }
 
-  return <div> 
-    <h1>{count}</h1>
-   </div>
- }
+  return <div>
+    <Counter count={count} />
+    <button onClick={increase}>Incrrease Count</button>
+    <button onClick={decrease}>Decrrease Count</button>
+  </div>
+    
+
+  }
+
+  function Counter(props) {
+    useEffect(function(){
+      console.log("mount");
+
+      return function() {
+        console.log("unmount")
+      }
+    }, []); 
+
+  useEffect(function() {
+    console.log("count has changed");
+  }, [props.count ])  
+
+   return <div>
+    Counter {props.count }
+    Counter {props.count2 }
+
+   </div> 
+  }
 
 export default App
