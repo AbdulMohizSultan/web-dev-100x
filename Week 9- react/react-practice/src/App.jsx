844bcfafac9123345fa2b26f895d6401771cbbbf
Abdul Mohiz Awan <142517175+AbdulMohizSultan@users.noreply.github.com>
2025-09-24 10:17:21 -0700

    Update App.jsx

--- Week 9- react/react-practice/src/App.jsx
+++ Week 9- react/react-practice/src/App.jsx
@@ -1,28 +1,49 @@
 function App() {
 
-  return  <div>
-    
-    <PostComponent></PostComponent>
-    
-    </div>
+  return(  
   
-}
+  <div style={{background: "#dfe6e9", height: "100vh",}} >   
+    <div style={{display:"flex", justifyContent: "center "}}>
+        <div>
+          <div>
+            <PostComponent/>
+            <br />
+         </div>
+          <div>
+              <PostComponent/>
+              <br />
+           </div>
+           <div>
+              <PostComponent/>
+              <br />
+            </div>  
+        </div>
+    </div>
+  </div>
+  
+)}
 
-const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1 }  
+const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20  }  
 
 function PostComponent() {
   return <div style={style}> 
-      <img src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fvectors%2Flogo-design-png&psig=AOvVaw2K9z5788oRgdIQe31UQz6G&ust=1758732277435000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLC5vYmt748DFQAAAAAdAAAAABAE"} style={{
+    <div style={{display: "flex"}}>
+      <img src={"https://cdna.artstation.com/p/assets/images/images/051/396/688/large/amir-kahory-joker-movie-poster.jpg?1657189570"} style={{
         width:20,
         height:20,
         borderRadius:20
       }} />
-      <div>
+      <div style={{fontSize: 10, marginLeft: 10}}>
         <b>
           100xdevs
         </b>
+        <div>23,888 followers</div>
+        <div>12m</div>
       </div>
-
+    </div>
+    <div style={{fontSize:12}} >
+       what to know how to win big? checkeeeeeeee out how these folks won $6000 in bounties
+    </div>
    </div> 
 }
 
